99ebe8404965ca41528f2b0704aafb1c3bac4371
Steve Taylor <steve@deployhub.com>
2023-08-07 13:20:58 -0600

    chg lint settings Signed-off-by: Steve Taylor <steve@deployhub.com>

--- main.py
+++ main.py
@@ -12,6 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# pylint: disable=E0401,E0611
+# pyright: reportMissingImports=false,reportMissingModuleSource=false
+
 import logging
 import os
 import socket
